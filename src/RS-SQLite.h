5d38d173b042cf43f49b4bdda27bd7359474227f
Seth Falcon <sfalcon@fhcrc.org>
2007-06-09 00:00:00 +0000

    version 0.6-0

--- src/RS-SQLite.h
+++ src/RS-SQLite.h
@@ -24,8 +24,8 @@
 extern  "C" {
 #endif
 
-#ifdef RSQLITE_USE_BUNDLED_SQLITE
-#  include "sqlite/sqlite3.h"
+#ifdef WIN32
+#  include "windows/sqlite3.h"
 #else
 #  include <sqlite3.h>
 #endif
--- src/sqlite-all.c
+++
@@ -1,3 +0,0 @@
-#ifdef RSQLITE_USE_BUNDLED_SQLITE || WIN32
-#  include "sqlite/sqlite3.c"
-#endif
